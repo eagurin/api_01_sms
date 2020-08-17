9f3dd1b493520960fa0636dca0bcdc21341ac7c3
Евгений Гурин <evgeny@MacBook-Air-Evgenij.local>
2020-08-18 00:24:03 +0300

    upload api_01_sms

--- homework.py
+++ homework.py
@@ -1,20 +1,42 @@
+import os
 import time
 
+import json
 import requests
 from twilio.rest import Client
+from dotenv import load_dotenv
+load_dotenv()
+
+
+account_sid = os.getenv('account_sid')
+auth_token = os.getenv('auth_token')
+access_token = os.getenv('access_token')
+NUMBER_FROM = os.getenv('NUMBER_FROM')
+NUMBER_TO = os.getenv('NUMBER_TO')
+client = Client(account_sid, auth_token)
 
 
 def get_status(user_id):
     params = {
-        ...
+        'access_token': access_token,
+        'user_ids': user_id,
+        'fields': 'online',
+        'v': '5.92'
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    response = requests.post(
+        'https://api.vk.com/method/users.get', params=params).json()
+    status = response['response'][0]['online']
+    return status
 
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    message = client.messages \
+        .create(
+            body=sms_text,
+            from_=NUMBER_FROM,
+            to=NUMBER_TO
+        )
+    return message.sid
 
 
 if __name__ == "__main__":
